9704bd888b3a70316620dbb6fa1b2326d9d2d238
Pavankalyan477 <sarakantipavankalyan070@gmail.com>
2022-01-19 21:51:30 +0530

    prime

---
+++ DSA/Ankushprob/CheckPrime/Prime.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Prime {
+    //INPUT : 7
+    // OUTPUT : Prime
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        // if(n==1){
+        //     System.out.println("No");
+        // }
+        // else{
+        //     int count=0;
+        //     for(int i=1;i<=n;i++){
+        //         if(n%i==0){
+        //             count++;
+        //         }
+        //     }
+        //     if(count>2){
+        //         System.out.println("No");
+        //     }else{
+        //         System.out.println("Yes");
+        //     }
+        // }
+        boolean flag=true;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                flag=false;
+                break;
+            }
+        }
+        if(flag==true){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
--- DSA/Ankushprob/FizzBuzz/Fizzbuzz.java
+++ DSA/Ankushprob/FizzBuzz/Fizzbuzz.java
@@ -1,15 +1,20 @@
 import java.util.*;
 class Fizzbuzz{
+
+    // Input : 10
+    // output:  1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             if(i%3==0 && i%5==0){
-                System.out.println("FizzBuzz");
+                System.out.print("FizzBuzz"+" ");
             }else if(i%3==0){
-                System.out.println("Fizz");
+                System.out.print("Fizz"+" ");
             }else if(i%5==0){
-                System.out.println("Buzz");
+                System.out.print("Buzz"+" ");
+            }else{
+                System.out.print(i+" ");
             }
         }
     }
