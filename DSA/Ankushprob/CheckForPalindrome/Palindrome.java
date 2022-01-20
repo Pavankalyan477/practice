7cdf68711a49bf43b7c0e852b707157093e3ced0
Pavankalyan477 <sarakantipavankalyan070@gmail.com>
2022-01-20 19:22:54 +0530

    palindrome

--- DSA/Ankushprob/CheckForPalindrome/Palindrome.java
+++ DSA/Ankushprob/CheckForPalindrome/Palindrome.java
@@ -1,3 +1,18 @@
+import java.util.*;
 public class Palindrome {
-    
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        sc.nextLine();
+        String str=sc.nextLine();
+        String st="";
+        for(int i=n-1;i>=0;i--){
+            st=st+str.charAt(i);
+        }
+        if(str.equals(st)){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
 }
