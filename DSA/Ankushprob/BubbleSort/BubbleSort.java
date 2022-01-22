19db73244762626cee87be7c0a6fcdeb7470896a
Pavankalyan477 <sarakantipavankalyan070@gmail.com>
2022-01-22 17:27:17 +0530

    quicksort

--- DSA/Ankushprob/BubbleSort/BubbleSort.java
+++ DSA/Ankushprob/BubbleSort/BubbleSort.java
@@ -7,10 +7,19 @@ public class BubbleSort {
             for(int i=0;i<n;i++){
                 a[i]=sc.nextInt();
             }
+            // [5 1 4 2 8]
+            //i=0; j=0,j+1 // 1 5 4 2 8
+            // 1 4 5 2 8
+            // 1 4 2 5 8
+
+            //1 2 4 5 8
+
+            
+
             for(int i=0;i<n;i++){
                 boolean flag=false;
-                for(int j=0;j<n-i-1;j++){
-                    if(a[j]>a[j+1]){
+                for(int j=0;j<n-1;j++){
+                    if(a[j]<a[j+1]){
                         int temp=a[j];
                         a[j]=a[j+1];
                         a[j+1]=temp;
--- DSA/Ankushprob/QuickSort/Quicksort.java
+++ DSA/Ankushprob/QuickSort/Quicksort.java
@@ -1,11 +1,38 @@
 import java.util.*;
 public class Quicksort {
+    static int partition(int[] a,int low,int high){
+        int pivot=a[high];
+        int i=low-1;
+        for(int j=low;j<high;j++){
+            if(a[j]<pivot){
+                i++;
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+        int var=a[i+1];
+        a[i+1]=a[high];
+        a[high]=var;
+        return i+1;
+    }
+    static void quicksort(int[] a,int low,int high){
+        if(low<high){
+            int index=partition(a,low,high);
+            quicksort(a, low, index-1);
+            quicksort(a,index+1,high);
+        }
+    }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int[] a=new int[n];
         for(int i=0;i<n;i++){
             a[i]=sc.nextInt();
+        }
+        quicksort(a,0,n-1);
+        for(int i=0;i<n;i++){
+           System.out.print(a[i]+" ");
         }
     }
 }
